1470a0ae1a9b7ad10b4e9a1fd2fda982cf73d97f
rebecca0814 <s10714114@gm.cyut.edu.tw>
2019-10-25 16:29:08 +0800

    chapter 6

--- blog/main/views.py
+++ blog/main/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 def main(request):
     '''
-    Show 'Hello world!' in the main page
+    Render the main page
     '''
-    return HttpResponse('Hello world!')
+    context = {'like':'Django 很棒'}
+    return render(request, 'main/main.html', context)
